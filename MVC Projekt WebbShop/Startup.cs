ceb4613bd72b735aa27a1fae4b1f7213ba13e6bc
ChrilleJ <christian.jarenfors@gmail.com>
2016-04-05 09:21:23 +0200

    Grunduppluppladdar Templaten

---
+++ MVC Projekt WebbShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Projekt_WebbShop.Startup))]
+namespace MVC_Projekt_WebbShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
