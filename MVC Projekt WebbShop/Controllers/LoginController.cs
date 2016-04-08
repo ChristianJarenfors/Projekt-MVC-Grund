13fd9460b4ba310c94a1f0cf9bc57d2a6b5fa373
ChrilleJ <christian.jarenfors@gmail.com>
2016-04-08 13:51:42 +0200

    Börjar med inloggen

---
+++ MVC Projekt WebbShop/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Projekt_WebbShop.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl();
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ MVC Projekt WebbShop/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Projekt_WebbShop.Models
+{
+    public class UserModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool EditAthority { get; set; }
+    }
+}
